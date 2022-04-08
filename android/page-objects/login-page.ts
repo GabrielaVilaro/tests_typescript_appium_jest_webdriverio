4af0060afa8ff07dfa1439f973461df879718e99
Gabriela Vilaró <50081357+GabrielaVilaro@users.noreply.github.com>
2022-04-08 11:51:18 -0300

    Create login-page.ts

---
+++ android/page-objects/login-page.ts
@@ -0,0 +1,16 @@
+import { Browser } from "webdriverio";
+class loginScreen {
+  public static locators = {
+    button1: "//*[@text='1']",
+    button2: "//*[@text='2']",
+    button3: "//*[@text='3']",
+    button4: "//*[@text='4']",
+    button5: "//*[@text='5']",
+    button6: "//*[@text='6']",
+    buttonView: "//*[@text='Ver más']"
+
+  }
+
+  
+}
+export default loginScreen;
